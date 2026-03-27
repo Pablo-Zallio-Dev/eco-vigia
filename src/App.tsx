3bd41153de841464ff445a79af822be878075044
Pablo Zallio <pablozalliodev@gmail.com>
2026-03-27 10:09:24 +0100

    [FEAT] - modifying styles

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ const App = () => {
       <IncidentForms />
       <Navbar />
 
+
       </>
 )
 }
--- src/components/IncidentForm/components/IncidentForms.tsx
+++ src/components/IncidentForm/components/IncidentForms.tsx
@@ -21,7 +21,7 @@ const IncidentForms = () => {
       console.log(errors)
 
       return (
-            <section className={`  absolute top-50  ${ !isFormOpen ? ' translate-y-full ' :  ''} left-0 z-1000 flex flex-col gap-6 w-full h-full p-4 bg-form text-label rounded-t-2xl transition-transform duration-150 `}>
+            <section className={`  absolute top-20  ${ !isFormOpen ? ' translate-y-full ' :  ''} left-0 z-1000 flex flex-col gap-6 w-full h-full p-4 bg-form text-label rounded-t-2xl transition-transform duration-500 `}>
                   <section className=" flex justify-around items-center ">
                         <h2 className=" text-xs text-center ">Reportar nuevo incidente</h2>
                         <CircleX size={16} color='var(--text-label)' onClick={closeForm} />
